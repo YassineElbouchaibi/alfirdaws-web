37dd7a1af7a71ce6327deaba33a7f402f543cc96
Yassine El Bouchaibi <yassine.el-bouchaibi@polymtl.ca>
2022-02-13 19:51:44 -0500

    Small tweaks

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Masjid Al Firdaws Storybook',
+  brandUrl: 'http://localhost:3000',
+  brandImage: null,
+  fontBase: '"Source Sans Pro"',
+});
