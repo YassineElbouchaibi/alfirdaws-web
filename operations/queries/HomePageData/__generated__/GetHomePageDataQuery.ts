4cb4698cf16f289dc524be99ad527f904d36a190
Yassine El Bouchaibi <yassine.el-bouchaibi@polymtl.ca>
2022-02-13 19:26:09 -0500

    Move GlobalTypes

--- operations/queries/HomePageData/__generated__/GetHomePageDataQuery.ts
+++ operations/queries/HomePageData/__generated__/GetHomePageDataQuery.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { PaginationArg } from './../../../../__generated__/globalTypes';
+import { PaginationArg } from './../../../../types/globalTypes';
 
 // ====================================================
 // GraphQL query operation: GetHomePageDataQuery
