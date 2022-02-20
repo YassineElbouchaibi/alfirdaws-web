5e387bb2fe811754d8d9812aab13f6c36f6e8537
Yassine El Bouchaibi <yassine.el-bouchaibi@outlook.com>
2022-02-20 17:37:38 -0500

    Env file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // -------- Strapi configuration --------
-const cmsUri = 'http://localhost:1338';
+const cmsUri = process.env.CMS_URI ?? 'http://localhost:1338';
 // --------------------------------------
 
 // -------- Moonode configuration --------
 // Found Endpoints:
 //   - https://fkil67m6y9.execute-api.ca-central-1.amazonaws.com/mosque/5142341340
 //   - https://vinp0qaxlb.execute-api.ca-central-1.amazonaws.com/get/odata/entity/AlFirdwas_MjI1OCFAIyQly4Ym
-const moonodeApiUri = 'https://fkil67m6y9.execute-api.ca-central-1.amazonaws.com/mosque';
-const moonodeMasjidKey = '5142341340';
+const moonodeApiUri = process.env.MOONODE_API_URI ?? 'https://fkil67m6y9.execute-api.ca-central-1.amazonaws.com/mosque';
+const moonodeMasjidKey = process.env.MOONODE_MASJID_KEY ?? '5142341340';
 // --------------------------------------
 
 /** @type {import('next').NextConfig} */
